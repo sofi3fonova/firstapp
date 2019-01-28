333a89db94a270319a320220ce1ce97721ae0eb6
sofi3fonova <3f.sofi@gmail.com>
2019-01-28 17:03:14 +0300

    first commit

---
+++ app/src/main/java/com/example/a3fsof/a21jan2019/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a3fsof.a21jan2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyView(this));
+    }
+}
